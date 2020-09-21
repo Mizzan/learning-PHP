1f5777b456d438e187adac279a6b9232838f995b
mizan <mizzanur007@gmail.com>
2020-09-21 14:33:38 +0600

    ✔ knowing all about Continue & Break -  in php

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
 <?php 
-  // Conditional Statements
-  // $price = 20;
-
-  // if($price < 10) {
-  //   echo 'the condition is met';
-  // } else if($price < 30) {
-  //   echo 'the else if condition is met';
-  // } else {
-  //   echo 'condition not met';
-  // }
-
-
 
   $products = [
     ['name' => 'red shell', 'price' => 5],
@@ -21,14 +9,15 @@
     ['name' => 'golden rod', 'price' => 2],
   ];
   
-  // foreach($products as $product) {
-    // if($product['price'] < 10 && $product['price'] > 2) {
-    //   echo $product['name'] . " - " . $product['price'] . '<br>';
-    // }
-    // if($product['price'] > 30 || $product['price'] < 10) {
-    //   echo $product['name'] . " - " . $product['price'] . '<br>';
-    // }
-  // }
+  foreach($products as $product) {
+    if($product['name'] === 'silver palate') {
+      break;
+    }
+    if($product['price'] > 10) {
+      continue;
+    }
+    echo $product['name'] . '<br>';
+  }
 
   // echo use to print out code 
   // echo "My first PHP code in the browser.";
@@ -44,18 +33,9 @@
   <title>String</title>
 </head>
 <body>
-  <h1>Conditional Statements</h1>
+  <h1>Continue & Break</h1>
     
-    <div>
-    <h3>Price less than 15</h3>
-      <ul>
-        <?php foreach($products as $product) { ?>
-          <?php if($product['price'] < 15) { ?>
-            <li><?php echo $product['name'] . ' - ' . $product['price'] ?></li>
-          <?php } ?>
-        <?php } ?>
-      </ul>
-    </div>
+   
 
 </body>
 </html>
