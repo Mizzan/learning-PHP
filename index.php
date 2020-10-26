704ad797e84f45b06f55b1cf3cfab1b9611629d2
mizan <mizzanur007@gmail.com>
2020-10-26 12:35:24 +0600

    ✔ lesson 10 - all about booleans & comparisions - in php

--- index.php
+++ index.php
@@ -1,53 +1,37 @@
 <?php 
-  // For loops
+  // Boolean & Comparisons
+
+  // echo true; 1
+  // echo false; null
+
+  // numbers 
+  // echo 5 < 10;
+
+  // echo 5 > 10;
   
+  // equality operator
+  // echo 5 == 5;
   
-  // for($i = 0; $i < 5; $i++) {
-  // echo 'some code';
-  // }
+  // not operator
+  // echo 5 != 10;
 
-  $players = ['messi', 'suarez', 'neymar'];
+  // greater than operator
+  // echo 5 >= 2;
 
-  // for($i = 0; $i < count($players); $i++) {
-  //   echo $players[$i] . '<br />';
-  // }
-
-  // using foreach
-  // foreach($players as $player) {
-  //   echo $player . '<br />';
-  // }
-
-  $products = [
-    ['name' => 'shiny star', 'price' => 11],
-    ['name' => 'green shell', 'price' => 33],
-    ['name' => 'red shell', 'price' => 21],
-    ['name' => 'gold coin', 'price' => 45],
-    ['name' => 'lightning bolt', 'price' => 35],
-    ['name' => 'banana skin', 'price' => 16],
-  ];
-
-  // foreach ( array looping)
-  // foreach($blogs as $blog) {
-    // echo 'some template';
-  //   // }
-  // foreach($products as $product) {
-  //   echo $product['name'] ." - ". $product['price'] . '<br>';
-  // }
-
-  // while loop
-
-  // $i = 0;
-
-  // while($i < count($products)) {
-  //   echo $products[$i]['name'] . " - " . $products[$i]['price'];
-  //   echo '<br>';
-  //   $i++;
-  // }
-
-
-
-
+  // less than operator
+  // echo 1 <= 2;
   
+  // small letter is greater than capital letter
+  echo 'mario' > 'Adam';
+
+
+  // loose vs strict equal comparison
+
+  echo 5 === 5;
+  echo 4 === 5;
+
+
+
   // echo use to print out code 
   // echo "My first PHP code in the browser.";
   // // always use semicolon;
@@ -62,16 +46,9 @@
   <title>String</title>
 </head>
 <body>
-  <h1>Products</h1>
+  <h1>Boolean & Comparisons</h1>
     
     
-    <ul>
-    <!-- this is a php template  -->
-      <?php foreach($products as $product) { ?>
-        <h3><?php echo $product['name']; ?></h3>
-        <p>💰 <?php echo $product['price']; ?></p>
-      <?php } ?>
-    </ul>
 
 </body>
 </html>
